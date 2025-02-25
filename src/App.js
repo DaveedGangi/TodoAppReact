d70e103355b4080de06b61c4d7e45e5e2baedfba
DaveedGangi <daveedgangi@gmail.com>
2025-02-25 20:28:21 +0530

    todoApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from "react";
+import AnimatedClock from "./components/animatedClock";
+import "./App.css";
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state={tasks:[],newTask:"",editCondition:false,editIndex:null,animation:true}
+
+  componentDidMount(){
+    const tasksFromLocal=JSON.parse(localStorage.getItem("tasks"));
+    if(tasksFromLocal){
+      this.setState({tasks:tasksFromLocal});
+    }
+    setTimeout(()=>{
+      this.setState({animation:false});
+    },4000);
+  }
+  addTask=()=>{
+    const{tasks,newTask,editCondition,editIndex}=this.state;
+    if(newTask.length>0){
+      const updatedTasks=[...tasks];
+      const addingTaskData={
+        id:tasks.length+1,
+        name:newTask,
+        completed:false
+      }
+    if(editCondition){
+    
+      
+      updatedTasks[editIndex]=addingTaskData;
+      this.setState({tasks:updatedTasks,editIndex:null,newTask:"",editCondition:!editCondition},()=> localStorage.setItem("tasks",JSON.stringify(this.state.tasks)));
+      
+    }else{
+     
+    this.setState({tasks:[...tasks,addingTaskData],newTask:""},()=> localStorage.setItem("tasks",JSON.stringify(this.state.tasks)));
+    
+    }
+    }
+  }
+
+  handleText=(e)=>{
+    this.setState({newTask:e.target.value});
+  }
+  deleteTask=(i)=>{
+    const{tasks} =this.state;
+    const filteredTasks=tasks.filter((each,index)=>{return index!==i})
+    this.setState({tasks:filteredTasks,newTask:"",editCondition:false},()=>localStorage.setItem("tasks",JSON.stringify(this.state.tasks)));
+    
+
+  }
+  editTask=(i)=>{
+    const{tasks,editCondition}=this.state;
+    
+    this.setState({newTask:tasks[i].name,editCondition:!editCondition,editIndex:i});
+
+  
+  }
+  completTask=(i)=>{
+      const{tasks}=this.state;
+      const updatedTasks=[...tasks];
+      updatedTasks[i].completed=!updatedTasks[i].completed;
+      this.setState({tasks:updatedTasks},()=>localStorage.setItem("tasks",JSON.stringify(this.state.tasks)));
+      
+  }
+  render(){
+    const{tasks,newTask,editCondition,animation}=this.state;
+    return(
+      <div className="App">
+     
+       {animation ?<AnimatedClock/>: 
+        
+        <div>
+        
+        <input value={newTask} onChange={this.handleText} type="text" placeholder="Write tasks"/>
+        <button className={editCondition?"updateButton":"addButton"} onClick={this.addTask}>{editCondition?"Update":"Add"}</button>
+        <br/>
+      
+        {
+          tasks.map((each,i)=>{
+            return <div className={each.completed?"true":"false"} key={i}><p>{each.name}</p>
+            <button className="deleteButton" type="button" onClick={()=>this.deleteTask(i)}>Delete</button>
+            &nbsp;<button className="editButton" type="button" onClick={()=>this.editTask(i)}>Edit</button>
+            <input className="checkBox" type="checkbox" checked={each.completed} onChange={()=>this.completTask(i)} /></div>
+          })
+        }
+
+        </div>
+
+      }
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/animatedClock.js
@@ -0,0 +1,47 @@
+import {Component} from "react";
+import "./index.css";
+class AnimatedClock extends Component{
+
+    state={time:new Date()};
+
+    componentDidMount(){
+        this.timer=setInterval(()=>{
+            this.setState({time:new Date()});
+        },1000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.timer);
+    }
+    render(){
+        const {time}=this.state;
+       
+        const seconds=time.getSeconds();
+        const minutes=time.getMinutes()+seconds/60;
+        const hours=time.getHours()%12+minutes/60;
+
+        return(
+            <div className="clock-container">
+             
+            <div className="clock">
+            <h1>Todo List App</h1>
+              <div
+                className="hand hour-hand"
+                style={{ transform: `rotate(${hours * 30}deg)` }}
+              />
+              <div
+                className="hand minute-hand"
+                style={{ transform: `rotate(${minutes * 6}deg)` }}
+              />
+              <div
+                className="hand second-hand"
+                style={{ transform: `rotate(${seconds * 6}deg)` }}
+              />
+              <div className="center-dot" />
+            </div>
+          </div>
+
+        )
+    }
+}
+
+export default AnimatedClock;
